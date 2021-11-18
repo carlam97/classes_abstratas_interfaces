0e0c046ceaa0fe044caff7a2aa722356cb860824
carlam97 <carmartins04@hotmail.com>
2021-11-18 19:51:51 +0000

    Classes abstratas e interfaces

---
+++ Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+    public static void main(String[] args) {
+        ContaEspecializada contaEspecializada = new ContaEspecializada();
+
+        contaEspecializada.setSaldo(25000);
+        contaEspecializada.imprimeExtrato();
+    }
+    
+}
